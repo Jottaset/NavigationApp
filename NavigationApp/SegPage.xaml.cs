9832fdcdbf9f9a048e796c086e91208c73db4990
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-11 20:03:24 -0400

    first commit

---
+++ NavigationApp/SegPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NavigationApp
+{
+    public partial class SegPage : ContentPage
+    {
+        void Voltar(object sender, System.EventArgs e)
+        {
+            //Navigation.PushAsync(new MainPage());
+            Navigation.PopAsync();
+        }
+
+        public SegPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
